4e3805767fe4a434836542cef307ee1695ebdf78
David Kim <davidjrkims@gmail.com>
2022-10-06 22:43:26 +0900

    initial start of project with create-react-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>3</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
